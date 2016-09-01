317ccfadcf899c05e0a51df44efad862dcb40a12
Matheus M. Lessa <matheusmlessa@gmail.com>
2016-09-01 13:41:29 -0300

    Cáp4 - Melhorando a experiência do usuário

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,13 +1,14 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
 	$scope.foto = [];
+	$scope.filtro = '';
 
 	$http.get('v1/fotos')
 	.success(function(fotos) {
 		$scope.fotos = fotos;
 	})
 	.error(function(erro) {
-		console.log(error);
+		console.log(erro);
 	});
 	
 	/*
